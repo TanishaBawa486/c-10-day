b70f874f61b62ba4341a1ca5307e5967aef25156
TanishaBawa486 <bawatanisha486@gmail.com>
2025-06-07 19:50:42 +0530

    first commit

---
+++ loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int nums[] ={5,15,22,1,78,90};
+    int size = 6;
+    int smallest =INT_MAX;
+    for( int i=0; i<size; i++){
+        if(nums[i] < smallest){
+            smallest=nums[i];
+        }
+    }
+    cout <<"smallest=" << smallest <<endl;
+    return 0;
+}
